de7b9615edd926994d15fb7f6bea5a33fa251ecd
DasAnindita <118145613+DasAnindita@users.noreply.github.com>
2024-03-11 22:16:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 document.getElementById("form").addEventListener("submit", function(event) {
     event.preventDefault();
 
-    var email = document.getElementById("email").value;
+    var email = document.getElementById("Email").value;
 var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (emailPattern.test(email)) {
         // Email is valid, show success message and clear the input field
         document.getElementById("successMessage").style.display = "block";
-        document.getElementById("email").value = "";
+        document.getElementById("Email").value = "";
     } else {
         // Email is not valid, you can show an error message or handle it accordingly
         alert("Please enter a valid email address.");
